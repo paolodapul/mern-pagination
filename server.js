e0bf904d32591d8c909d46504b5f46d323bc5d6e
Paolo Dapul <markpaolodapul@gmail.com>
2021-02-15 13:36:52 +0800

    Setup frontend and backend including database

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config({ path: './config.env' });
+const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
